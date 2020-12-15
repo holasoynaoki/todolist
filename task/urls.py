4b4f6c84a532b768203f148894cafbc38ef9d722
Naoki Shigihara <75709599+holasoynaoki@users.noreply.github.com>
2020-12-15 14:13:27 +0900

    Update urls.py
    Deleted unnecessary rows

--- task/urls.py
+++ task/urls.py
@@ -4,12 +4,9 @@ from .views import *
 # from .views import index, todo_post, delete
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('todo_post/', todo_post, name='todo_post'),
     path('delete/<str:task_id>', delete, name='delete'),
     path('update/<str:task_id>', update, name='update'),
     path('edit/<str:task_id>', edit, name='edit'),
-    # path('is_edit/<str:task_id>', is_edit, name='is_edit'),
-    # path('vamos/<str:task_id>', vamos, name='vamos'),
 ]
