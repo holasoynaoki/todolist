236dd7b90a56642b8dc6198ce84b2ea247a8cd1e
Naoki Shigihara <75709599+holasoynaoki@users.noreply.github.com>
2020-12-15 14:14:32 +0900

    Update urls.py
    Deleted unnecessary rows

--- todo_django/urls.py
+++ todo_django/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
     path('', include('task.urls')),
     path('todo_post/', include('task.urls')),
     path('delete/<str:task_id>', include('task.urls')),
